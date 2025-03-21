6fb7fdf046f0dcf29b35a394502ab3c5d530289c
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2025-03-21 11:59:21 +0100

    Hacer modificar Recompensa

--- Backend/src/main/java/com/copernic/backend/Backend/entity/PuntBescanvi.java
+++ Backend/src/main/java/com/copernic/backend/Backend/entity/PuntBescanvi.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@ToString
 public class PuntBescanvi {
 
     @Id
@@ -34,5 +36,6 @@ public class PuntBescanvi {
     private String observacions;
 
     @OneToMany(mappedBy = "puntBescanviId", cascade = CascadeType.ALL)
+    @ToString.Exclude
     private List<Recompensas> puntBescanviID;
 }
--- Backend/src/main/java/com/copernic/backend/Backend/logic/web/PuntBescanviLogic.java
+++ Backend/src/main/java/com/copernic/backend/Backend/logic/web/PuntBescanviLogic.java
@@ -1,4 +1,24 @@
 package com.copernic.backend.Backend.logic.web;
 
+import com.copernic.backend.Backend.entity.PuntBescanvi;
+import com.copernic.backend.Backend.entity.Recompensas;
+import com.copernic.backend.Backend.repository.BescanviRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PuntBescanviLogic {
+
+    @Autowired
+    BescanviRepository bescanviRepository;
+
+    public List<PuntBescanvi> llistarBescanvi() {
+        return bescanviRepository.findAll();
+    }
+
+    public PuntBescanvi findByID(Long id) {
+        return bescanviRepository.findById(id).get();
+    }
 }
