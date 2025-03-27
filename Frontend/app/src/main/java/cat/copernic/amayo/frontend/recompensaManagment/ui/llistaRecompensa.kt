5b286ba4a311327593ded7c00963f5675b075988
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2025-03-27 13:10:24 +0100

    Crear Bottom Navigation en el Android

--- Frontend/app/src/main/java/cat/copernic/amayo/frontend/recompensaManagment/ui/llistaRecompensa.kt
+++ Frontend/app/src/main/java/cat/copernic/amayo/frontend/recompensaManagment/ui/llistaRecompensa.kt
@@ -1,2 +1,9 @@
 package cat.copernic.amayo.frontend.recompensaManagment.ui
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun recompensa(){
+    Text("Recompensa")
+}
---
+++ Frontend/app/src/main/java/cat/copernic/amayo/frontend/usuariManagment/ui/PerfilScreen.kt
@@ -0,0 +1,9 @@
+package cat.copernic.amayo.frontend.usuariManagment.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun perfil(){
+    Text("perfil")
+}
