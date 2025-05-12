1d142020c6cce7cf07368e45fab616c0c3ba45a5
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2025-05-12 13:21:45 +0200

    Test Reservas

--- Backend/src/main/java/com/copernic/backend/Backend/logic/android/api/ApiServiceReserva.java
+++ Backend/src/main/java/com/copernic/backend/Backend/logic/android/api/ApiServiceReserva.java
@@ -2,13 +2,28 @@ package com.copernic.backend.Backend.logic.android.api;
 
 import com.copernic.backend.Backend.entity.Reserva;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface ApiServiceReserva {
 
-    final String BASE_URL = "/api/reserva/";
+    final String BASE_URL = "/api/reserva";
+    @POST(BASE_URL + "/crear")
+    Call<Long> save(@Body Reserva reserva);
 
-    @POST(BASE_URL+"crear")
-    Call<Long> save(@Body Reserva prod);
+    /* ---------- READ ---------- */
+    @GET(BASE_URL + "/all")
+    Call<List<Reserva>> findAll();
+
+    @GET(BASE_URL + "/byId/{id}")
+    Call<Reserva> findById(@Path("id") Long id);
+
+    /* ---------- UPDATE ---------- */
+    @PUT(BASE_URL + "/update")
+    Call<Void> update(@Body Reserva reserva);
+
+    /* ---------- DELETE ---------- */
+    @DELETE(BASE_URL + "/delete/{id}")
+    Call<Void> deleteById(@Path("id") Long id);
 }
